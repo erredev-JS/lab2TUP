2623365f4a819fe70136b87a3990ee924e413186
Rodrigo Zapata <118647241+erredev-JS@users.noreply.github.com>
2024-04-10 18:19:10 -0300

    Delete Excepciones directory

--- Excepciones/ExcepcionesPropias/ExcepcionAlturaFueraDeRango.java
+++
@@ -1,7 +0,0 @@
-package Excepciones.ExcepcionesPropias;
-
-public class ExcepcionAlturaFueraDeRango extends Exception {
-  public ExcepcionAlturaFueraDeRango() {
-    System.out.println("ExcepcionAlturaFueraDeRango: La altura está fuera del SOS rango permitido.");
-  }
-}
--- Excepciones/ExcepcionesVarias/EjemploExcepciones07SinThrow.java
+++
@@ -1,8 +0,0 @@
-package Excepciones.ExcepcionesVarias;
-
-public class EjemploExcepciones07SinThrow {
-  public static void main(String[] args) {
-    System.out.println("Inicio");
-    System.out.println(1 / 0);
-  }
-}
