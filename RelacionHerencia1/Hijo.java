8986cd6c77e76cc1c11a1978661262e3c5b11c7f
Rodrigo Zapata <118647241+erredev-JS@users.noreply.github.com>
2024-04-10 18:19:43 -0300

    Delete RelacionHerencia1 directory

--- RelacionHerencia1/Hijo.java
+++
@@ -1,9 +0,0 @@
-package RelacionHerencia1;
-
-public class Hijo extends Padre{
-
-    public Hijo(int edad) {
-        super(edad);
-        System.out.println("Se creó el hijo y su edad es " + edad);
-    }
-}
